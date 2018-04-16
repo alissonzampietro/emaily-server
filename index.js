04926e57cbec7a8605dc18a143e0393c55f166ed
Alisson Zampietro <alisson.zampietro@gmail.com>
2018-04-16 01:44:55 -0300

    add heroku port

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 5000; 
 
 app.get('/',(req, res) => {
   res.send({message: 'Bem vindo'});
   console.log(req);
 });
+
 
 app.listen(5000);
