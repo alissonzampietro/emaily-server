555aefeaf4181e701e593b4c2ade5489935b1396
Alisson Zampietro <alisson.zampietro@gmail.com>
2018-06-26 08:59:45 -0300

    [FEAT] add googleauth

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const {googleClientID, googleClienteSecret} = require('./config/keys.js')
 const app = express();
-const PORT = process.env.PORT || 8888;
 
-app.get('/',(req, res) => {
-  res.send({data: 'Let\'s be friends'});
-  console.log(req);
+// clientID 349506072031-gk5s2o71421bsmnran5r4vma76lo8d5k.apps.googleusercontent.com
+// clienteSecret 12kUP-dtaUdemLya77uhe-vg
+passport.use(new GoogleStrategy(
+  {
+    clientID: googleClientID,
+    clienteSecret: googleClienteSecret,
+    callbackURL: '/auth/google/callback'
+  },
+  (accessToken) => {
+  console.log(accessToken);
+  }
+));
+
+app.get('/auth/google',
+        passport.authenticate('google', {
+          scope: ['profile','email']
+        })
+);
+
+app.get('/auth/google/callback', (req,res) => {
+
 });
 
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT);
