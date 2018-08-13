8f10be5313d735c827e87182143d88c81ce9b561
Alisson Zampietro <alisson.zampietro@gmail.com>
2018-08-13 02:21:13 -0300

    feat: implementing routes, services and improving organization of coe

--- index.js
+++ index.js
@@ -1,30 +1,9 @@
 const express = require('express');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const {googleClientID, googleClientSecret} = require('./config/keys.js')
 const app = express();
+require('./services/passport');
+require('./routes/authRoutes')(app);
 
-passport.use(new GoogleStrategy(
-  {
-    clientID: googleClientID,
-    clientSecret: googleClientSecret,
-    callbackURL: '/auth/google/callback'
-  },
-  (accessToken, refreshToken, profile, done) => {
-    console.log({accessToken});
-    console.log({refreshToken});
-    console.log({profile});
-    console.log({done});
-  }
-));
 
-app.get('/auth/google',
-        passport.authenticate('google', {
-          scope: ['profile','email']
-        })
-);
-
-app.get('/auth/google/callback', passport.authenticate('google'));
 
 const PORT = process.env.PORT || 5000;
 
---
+++ services/passport.js
@@ -0,0 +1,19 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const {googleClientID, googleClientSecret} = require('./../config/keys.js')
+
+passport.use(new GoogleStrategy(
+    {
+        clientID: googleClientID,
+        clientSecret: googleClientSecret,
+        callbackURL: '/auth/google/callback'
+    },
+    (accessToken, refreshToken, profile, done) => {
+        console.log({accessToken});
+        console.log({refreshToken});
+        console.log({profile});
+        console.log({done});
+    }
+));
+
+module.exports = passport;
